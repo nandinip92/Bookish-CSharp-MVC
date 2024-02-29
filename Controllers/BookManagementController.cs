cd7c011a4d83e223c6c48fd00c0540f88a95afe7
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-29 17:00:06 +0000

    feat: Adding search feature

--- Controllers/BookManagementController.cs
+++ Controllers/BookManagementController.cs
@@ -14,18 +14,37 @@ public class BookManagement : Controller
     public IActionResult Index(int page = 0)
     {
         const int PageSize = 20;
+        var Search = new List<int> { 1, 2, 3, 4, 5 };
         var booksList = new BooksCatalog().BooksList;
+        booksList = booksList.OrderBy(title => title.BookTitle).ToList();
         var count = booksList.Count();
-        Console.WriteLine($"****************{count}");
         var booksData = booksList.Skip(page * PageSize).Take(PageSize).ToList();
         ViewBag.MaxPage = (count / PageSize) - (count % PageSize == 0 ? 1 : 0);
         ViewBag.Page = page;
         return View(booksData);
     }
 
-    public IActionResult Search()
+    // [HttpPost]
+    // public IActionResult ProcessForm(string SearchString)
+    // {
+    //     // Process the form data (e.g., validate, save to database, etc.)
+    //     // Redirect to a different URL after processing
+    //     return RedirectToAction("Search", new { SearchString });
+    // }
+
+    // [HttpGet("[controller]/{searchTitle}")]
+    public IActionResult Search(string searchTitle = "")
     {
-        return View();
+        var booksCatalog = new BooksCatalog();
+        var book = booksCatalog.SearchBook(searchTitle);
+        // if (searchTitle == "")
+        // {
+            return View(book);
+        // }
+        // else
+        // {
+        //     return View("~/Views/BookManagement/BookDetails.cshtml", book);
+        // }
     }
 }
 
--- Models/BooksCatalog.cs
+++ Models/BooksCatalog.cs
@@ -2,34 +2,46 @@ using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.TypeConversion;
+
 namespace Bookish.Models;
-public class BooksCatalog{
 
-    public  List<Book> BooksList{get;private set;}=[];
+public class BooksCatalog
+{
+    public List<Book> BooksList { get; private set; } = [];
+    public Dictionary<string, Book> BooksLookup { get; private set; } = [];
 
-    public BooksCatalog(){
-        ReadBooksDataFromCSVFile("Data/books.csv");
+    public BooksCatalog()
+    {
+        ReadBooksDataFromCSVFile("Data/BooksTest.csv");
     }
 
-    public void ReadBooksDataFromCSVFile(string fileName){
-        try{
+    public void ReadBooksDataFromCSVFile(string fileName)
+    {
+        try
+        {
             using var fileReader = new StreamReader(fileName);
-             var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                ReadingExceptionOccurred = args=>{
-                    if (args.Exception is TypeConverterException){
+                ReadingExceptionOccurred = args =>
+                {
+                    if (args.Exception is TypeConverterException)
+                    {
                         // _logger.LogWarning($"Bad Record Found {args.Exception.Context.Parser.RawRecord}");
                         return false;
                     }
                     return true;
                 }
             };
-            using var csvData = new CsvReader(fileReader,csvConfig);
-            foreach(var book in csvData.GetRecords<Book>()){
+            using var csvData = new CsvReader(fileReader, csvConfig);
+            foreach (var book in csvData.GetRecords<Book>())
+            {
                 BooksList.Add(book);
+                BooksLookup[book.BookTitle] = book;
             }
             Console.WriteLine("Loaded the Books Data.........");
-        }catch(FileNotFoundException){
+        }
+        catch (FileNotFoundException)
+        {
             Console.WriteLine($"Sorry, {fileName} was not found");
         }
         // } catch (IOException)
@@ -38,8 +50,20 @@ public class BooksCatalog{
         //         $"Sorry cannot open {fileName}, Please close the file if it is open in another process."
         //     );
         // }
-
     }
-    
 
+    public Book? SearchBook(string title)
+    {
+        if (BooksLookup.ContainsKey(title))
+        {
+            return BooksLookup[title];
+        }
+        return null;
+    }
+    // public T SearchBook<T>(string title)
+    //     if(BooksLookup.ContainsKey(title)){
+    //         return BooksLookup[title];
+    //     }
+    //     return "No Book Found";
+    // }
 }
